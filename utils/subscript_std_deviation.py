7c26cc9824e14cb73ec58b9113d83520d4865f56
rsahleanu <raresmagbares@gmail.com>
2025-01-18 19:35:27 +0100

    Standart deviation

--- utils/subscript_std_deviation.py
+++ utils/subscript_std_deviation.py
@@ -1,28 +1,26 @@
 import pandas as pd
 import argparse
 
-
 def main(args):
-        df = pd.read_csv(args.file)
+    # CSV einlesen
+    df = pd.read_csv(args.file)
 
-      # Standardabweichung berechnen
-        std = df["raw"].std()
+    # Standardabweichung berechnen
+    std = df["raw"].std()
 
-        # Standardabweichung in DataFrame umwandeln
-        data = pd.DataFrame({"stddev": [std]})
+    # Spalte 'features' prüfen und ggf. hinzufügen
+    if "features" not in df.columns:
+        df["features"] = ""  # Statt None leere Strings einfügen
 
-        # In die Datei schreiben oder anhängen
-        data.to_csv(args.file, mode='r+', index=False, header=False)
+    # Standardabweichung in die erste Zelle der Spalte schreiben
+    df.loc[0, "features"] = std
 
+    # Debugging-Ausgaben
+    print("DataFrame vor dem Speichern:")
+    print(df)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="arguments")
-    parser.add_argument("--file", type=str, required=True, help="The location of the file")
-
-    args = parser.parse_args()
-    main(args)
-
-
+    # Änderungen in die Datei schreiben
+    df.to_csv(args.file, index=True, header=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="arguments")
